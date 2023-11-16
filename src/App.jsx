902157d97eb38874d165a2fcac0d62c68b85cdb7
proteanmetamorphoses <steve.kurowski@gmail.com>
2023-11-15 20:40:22 -0700

    Fetch and map

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useEffect, useState } from "react";
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json",).then((response)=> response.json()).then((data) => setProducts(data));
+    console.log("useEffect ended");
+  },[]);
+const list = products.map((product) => <li key ={product.name}>{product.name}</li>);
+  
+return (
+  <>
+  Hello. 
+  <div>
+    <ul>{list}</ul>
+  </div>
+  </>
+  );
 }
 
-export default App
+export default App;
